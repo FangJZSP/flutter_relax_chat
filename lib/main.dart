8787a91f608f4baf299050b24aac1027cadc92ec
chengyu <>
2025-03-27 15:54:28 +0800

    修改状态栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app.dart';
 import 'manager/global_manager.dart';
 import 'manager/socket/socket_manager.dart';
@@ -8,6 +9,12 @@ import 'manager/database/dao_manager.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   WidgetsBinding.instance.deferFirstFrame();
+  // 设置状态栏样式
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // 状态栏背景色设为透明
+    statusBarIconBrightness: Brightness.dark, // 状态栏图标为黑色
+    statusBarBrightness: Brightness.light, // iOS状态栏亮度
+  ));
   launchApp();
 }
 
